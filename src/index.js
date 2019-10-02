55c3009240ac702f12e4b704a0eb453c4a5a73d5
Andrew <Deadline2020@yandex.by>
2019-10-02 23:18:56 +0300

    feat: add solution task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 module.exports = function zeros(expression) {
-  // your solution
+
+  let counters = {
+    count2: 0,
+    count5: 0,
+  };
+  expression.split("*").forEach(factorial => {
+    if (factorial.slice(-2) === "!!") {
+      for (let i = parseInt(factorial); i > 3; i -= 2) {
+        incCounters(i, counters);
+      }
+    } else {
+      for (let i = parseInt(factorial); i > 1; i--) {
+        incCounters(i, counters);
+      }
+    }
+  });
+  return counters.count2 < counters.count5 ? counters.count2 : counters.count5
+}
+
+let incCounters = (number, counters) => {
+  for (; ;) {
+    if (!(number % 2)) {
+      counters.count2++;
+      number /= 2;
+      continue;
+    }
+    if (!(number % 5)) {
+      counters.count5++;
+      number /= 5;
+      continue;
+    }
+    break;
+  }
 }
